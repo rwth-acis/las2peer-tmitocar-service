c3fd424113a1ffd619f653120ffa34ea599620a7
Aran30 <50965489+Aran30@users.noreply.github.com>
2021-12-04 14:27:45 +0100

    fixed previous commit

--- las2peer-tmitocar-service/src/main/java/i5/las2peer/services/tmitocar/TmitocarService.java
+++ las2peer-tmitocar-service/src/main/java/i5/las2peer/services/tmitocar/TmitocarService.java
@@ -695,7 +695,7 @@ public class TmitocarService extends RESTService {
 				// response.put("fileType", "json");
 				response.put("fileType", "json");
 				response.put("fileName", userFileName.get(jsonBody.getAsString("channel")).replace(".txt","").replace(".pdf","")+"-graph");
-				if(!jsonBody.getAsString("submissionSucceeded") == null && !jsonBody.getAsString("submissionSucceeded").equals(""))
+				if(jsonBody.getAsString("submissionSucceeded") != null && !jsonBody.getAsString("submissionSucceeded").equals(""))
 				{
 					response.put("text", jsonBody.getAsString("submissionSucceeded"));
 				}
