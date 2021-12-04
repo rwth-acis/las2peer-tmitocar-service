1a191448e0214ad0ead2496d8d397b36cd7b584f
Aran30 <50965489+Aran30@users.noreply.github.com>
2021-12-04 13:57:22 +0100

    fixed naming json file

--- las2peer-tmitocar-service/src/main/java/i5/las2peer/services/tmitocar/TmitocarService.java
+++ las2peer-tmitocar-service/src/main/java/i5/las2peer/services/tmitocar/TmitocarService.java
@@ -694,7 +694,7 @@ public class TmitocarService extends RESTService {
 				response.put("fileBody", fileBody);
 				// response.put("fileType", "json");
 				response.put("fileType", "json");
-				response.put("fileName", userFileName.get(jsonBody.getAsString("channel")));
+				response.put("fileName", userFileName.get(jsonBody.getAsString("channel")).replace(".txt","").replace(".pdf","")+"-graph");
 				response.put("text", jsonBody.getAsString("submissionSucceeded"));
 			
 					//xAPImobsos.put("statement", xAPI);
