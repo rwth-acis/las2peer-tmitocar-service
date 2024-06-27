402cd58ede4ddfeaad331ccce22eb52d14bcd0b7
Yinnii <y.yin94@outlook.de>
2024-06-27 19:50:14 +0200

    update Configuration for MongoDB

--- tmitocar-service/src/main/java/services/tmitocar/config/TmitocarServiceConfig.java
+++ tmitocar-service/src/main/java/services/tmitocar/config/TmitocarServiceConfig.java
@@ -1,5 +1,22 @@
 package services.tmitocar.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
 public class TmitocarServiceConfig {
     
+    @Value("${spring.data.mongodb.uri}")
+    private String connString;
+
+    /*
+    * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
+    */
+    public @Bean MongoClient mongoClient() {
+        return MongoClients.create(connString);
+    }
 }
