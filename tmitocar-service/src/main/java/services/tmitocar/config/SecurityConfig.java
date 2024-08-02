13deada2405f26d09e16d3d0e6454877989298fc
Yinnii <y.yin94@outlook.de>
2024-08-02 17:09:00 +0200

    update securtiy config

--- tmitocar-service/src/main/java/services/tmitocar/config/SecurityConfig.java
+++ tmitocar-service/src/main/java/services/tmitocar/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package services.tmitocar.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
